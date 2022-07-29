dd2ec3ea0a73aba14800aba201e526ee89f18e31
carlosmbinf <carlosmbinf@gmail.com>
2022-07-29 17:11:07 -0400

    mejorando las rutas

--- server/rutas.js
+++ server/rutas.js
@@ -11,7 +11,60 @@ const endpoint = router();
 //   } from "../imports/ui/pages/collections/collections";
 
 if (Meteor.isServer) {
+  endpoint.get("/removegateway", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
 
+      await Meteor.call("removeGateway", req.query.serialnumber, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
+  endpoint.post("/removegateway", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
+      
+      await Meteor.call("removeGateway", req.query.serialnumber, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
   endpoint.get("/addgateway", async (req, res) => {
     // console.log(req)
     try {
@@ -43,8 +96,7 @@ if (Meteor.isServer) {
     }
 
   });
-
-  endpoint.post("/addperipheral", async (req, res) => {
+  endpoint.post("/addgateway", async (req, res) => {
     // console.log(req)
     try {
       console.log(req.query)
@@ -54,6 +106,122 @@ if (Meteor.isServer) {
         ip4: req.query.ip4
       }
       await Meteor.call("addGateway", gateway, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
+  endpoint.get("/removeperipheral", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
+      await Meteor.call("removePeripheral",  req.query.id, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
+  endpoint.post("/removeperipheral", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
+      await Meteor.call("removePeripheral",  req.query.id, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
+
+  endpoint.get("/addperipheral", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
+      let serialnumber = req.query.serialnumber
+      let peripheral = {
+        uid: req.query.uid,
+        vendor: req.query.vendor
+      }
+      await Meteor.call("addPeripheral", peripheral, serialnumber, function (error, mensaje) {
+        if (error) {
+          console.log(error);
+          res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+        } else {
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify({ result: mensaje }))
+        }
+      })
+      // res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+      //   res.end(req.query.idPeli);
+
+
+
+    } catch (error) {
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ error: error.message }))
+    }
+
+  });
+
+  endpoint.post("/addperipheral", async (req, res) => {
+    // console.log(req)
+    try {
+      console.log(req.query)
+      let serialnumber = req.query.serialnumber
+      let peripheral = {
+        uid: req.query.uid,
+        vendor: req.query.vendor
+      }
+      await Meteor.call("addPeripheral", peripheral, serialnumber, function (error, mensaje) {
         if (error) {
           console.log(error);
           res.setHeader('Content-Type', 'application/json')
